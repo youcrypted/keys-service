3fbf9b5f5e140f61859d99a4ab8b18bcfabd1f6d
kyberneees <kyberneees@kybpc>
2015-01-16 19:18:59 -0500

    Removing unrequired events callback. Refactoring app bootstrap.

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
+App.setDescription("User's RSA keys service for TESN");
+
 App.importScript('${APP_HOME}/config');
-
 App.on('appLoaded', function () {
-	App.setDescription("User's RSA keys service for TESN");
-
 	// loading services
 	App.importScript('${APP_HOME}/service/database');
 
 	// loading controllers
 	App.importScript('${APP_HOME}/controller/keys');
-});
-
-App.on(['beforeAppReload', 'systemStopping'], function () {
-	// releasing resources
-	DatabaseService.shutdown();
 });
